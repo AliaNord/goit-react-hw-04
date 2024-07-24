0fb14a9b14ff38eb87f6d19614c541d5581cf353
AliaNord <alia.nord.adventurer@gmail.com>
2024-07-24 13:54:42 +0200

    meow

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,13 +1,15 @@
+import s from "./SearchBar.module.css";
+
 export const SearchBar = ({ onSubmit }) => {
-  const formSubmit = e => {
+  const formSubmit = (e) => {
     e.preventDefault();
-    const{value} = e.target.elements.search;
-    onSubmit(value)
-  }
+    const { value } = e.target.elements.search;
+    onSubmit(value);
+  };
   return (
-    <form>
-      <input type="text" />
-      <button type="submit" onSubmit={formSubmit}></button>
+    <form className={s.searchForm} onSubmit={formSubmit}>
+      <input type="text" name="search" />
+      <button type="submit">Search</button>
     </form>
-  )
-}
+  );
+};
